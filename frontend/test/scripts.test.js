fbc85eef19aee5185fc4f56729f5c9b5267e33da
magsimal <max_blau@gmx.de>
2025-06-16 14:49:02 +0200

    Add tests for script loading and search overlay

---
+++ frontend/test/scripts.test.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const path = require('path');
+
+describe('index.html scripts', () => {
+  test('includes required script tags', () => {
+    const html = fs.readFileSync(path.join(__dirname, '../index.html'), 'utf8');
+    const scripts = [
+      'app.js',
+      'src/utils/exportSvg.js',
+      'src/utils/assignGenerations.js',
+      'src/utils/gedcom.js',
+      'src/config.js',
+      'flow.js',
+      'search.js',
+    ];
+    scripts.forEach((src) => {
+      expect(html).toContain(`<script src="${src}"></script>`);
+    });
+  });
+});
